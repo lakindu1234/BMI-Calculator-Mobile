6893edaaf21001239156dc108e5e3ddc0f2cc846
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-06-05 15:58:51 +0530

    BMI Calculator project Mobile

---
+++ lib/main_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Mainpage extends StatefulWidget {
+  const Mainpage({super.key});
+
+  @override
+  State<Mainpage> createState() => _MainpageState();
+}
