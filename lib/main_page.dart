5f5d9bb859bf15533a34adfaaf15315c7be94e7b
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-06-05 16:02:59 +0530

    Identify Variable

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -6,3 +6,11 @@ class Mainpage extends StatefulWidget {
   @override
   State<Mainpage> createState() => _MainpageState();
 }
+
+class _MainpageState extends State<Mainpage> {
+  int height = 150;
+  int weight = 70;
+
+  String gender = '';
+
+  late double bmi = calculateBMI(height: height, weight: weight);
