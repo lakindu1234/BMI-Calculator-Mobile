4f908cdf237360d72746e33994eb9cb4c482fc5a
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-06-20 22:10:15 +0530

    Input Colum

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 
 const kTextcolor = Color(0xFFFF5555);
 const kOutputTextcolor = Color(0xFF88BBFF);
+
+const kInputLableColor = TextStyle(
+  color: kTextcolor,
+  fontSize: 70,
+  fontWeight: FontWeight.bold,
+);
